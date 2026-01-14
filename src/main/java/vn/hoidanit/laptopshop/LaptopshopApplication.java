7a3776d0e0125ff0063b45fb3653a8527ffc2fa7
minhduyhodev <duyhm.ce190378@gmail.com>
2026-01-14 11:08:35 +0700

    update

--- src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -4,14 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
+// @SpringBootApplication
+@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext hoidanit = SpringApplication.run(LaptopshopApplication.class, args);
-		for (String s : hoidanit.getBeanDefinitionNames()) {
-			System.out.println(s);
-		}
+		SpringApplication.run(LaptopshopApplication.class, args);
+		// for (String s : hoidanit.getBeanDefinitionNames()) {
+		// System.out.println(s);
+		// }
 	}
 
 }
