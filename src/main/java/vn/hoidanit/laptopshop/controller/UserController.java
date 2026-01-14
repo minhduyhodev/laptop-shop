ab0d8210548d993d36e425ef137080e8204b44c8
minhduyhodev <duyhm.ce190378@gmail.com>
2026-01-14 23:40:34 +0700

    update

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,22 +1,14 @@
 package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.laptopshop.service.UserService;
 
-// @Controller
-// public class UserController {
-
-//     @RequestMapping("/")
-//     public String getHomePage() {
-//         return "Hello World";
-//     }
-// }
-
-@RestController
+@Controller
 public class UserController {
 
     private UserService userService;
@@ -25,8 +17,26 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("")
-    public String getHomePage() {
-        return this.userService.handleHelllo();
+    @RequestMapping("/")
+    public String getHomePage(Model model) {
+        String test = this.userService.handleHelllo();
+        model.addAttribute("eric", test);
+        model.addAttribute("duy", "minh duy dep trai");
+        return "hello";
     }
 }
+
+// @RestController
+// public class UserController {
+
+// private UserService userService;
+
+// public UserController(UserService userService) {
+// this.userService = userService;
+// }
+
+// @GetMapping("")
+// public String getHomePage() {
+// return this.userService.handleHelllo();
+// }
+// }
---
+++ src/main/webapp/WEB-INF/view/hello.jsp
@@ -0,0 +1,20 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    hello from jsp
+    <h1>
+        ${eric}
+    </h1>
+    <h2>
+        ${duy}
+
+    </h2>
+</body>
+
+</html>
